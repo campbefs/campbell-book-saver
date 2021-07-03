753e6ae5027c05a1dc575f946914fa866487c987
Christopher Campbell <campbefs@gmail.com>
2021-07-03 12:28:56 -0700

    updated gitignores in main

---
+++ client/build/precache-manifest.df0ac28f2e4f126ca2793a82a82187ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bbfc09f41a1d474083b94b71cfd713b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3dfd32e6fcbb9ddfc803",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "e5b4cf76da54fab02fd6",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "3dfd32e6fcbb9ddfc803",
+    "url": "/static/js/2.27b3ac98.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.27b3ac98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5b4cf76da54fab02fd6",
+    "url": "/static/js/main.0be71adc.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  }
+]);
